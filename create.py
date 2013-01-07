db12eb46a0e855d8dfb5d2a9fbde05ecb2b30146
Nader Morshed <morshed@ocf.berkeley.edu>
2013-01-06 23:24:48 -0800

    Moving stuff into create.py

--- create.py
+++ create.py
@@ -4,7 +4,42 @@ Let there be light.
 User creation tool.
 """
 
+import ldap
 import sys
+
+ACCOUNT_CREATED_LETTER="txt/acct.created.letter"
+
+def _associate_calnet(username):
+    pass
+
+def _check_username(username):
+    pass
+
+def _forward_add(username):
+    pass
+
+def _homedir_add(username):
+    pass
+
+def _kerberos_add(username):
+    pass
+
+def _sendemail(username):
+    pass
+
+def _email_problems():
+    pass
+
+def _finish_account_creation():
+    pass
+
+def _get_max_uid_number():
+	l = ldap.initialize("ldaps://ldap-master.ocf.berkeley.edu")
+	l.simple_bind_s('','')
+	ldap_entries = l.search_st("ou=People,dc=OCF,dc=Berkeley,dc=EDU", ldap.SCOPE_SUBTREE, "(uid=*)", ["uidNumber"])
+	uid_numbers = [max([int(num) for num in entry[1]["uidNumber"]]) for entry in ldap_entries]
+	max_uid_number = max(uid_numbers)
+	return max_uid_number
 
 def main(args):
     """
