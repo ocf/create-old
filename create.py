05e54bafed1c7fc18668e7d4ea7c7f54c76e1990
Nader Morshed <morshed@ocf.berkeley.edu>
2013-01-06 22:42:37 -0800

    Added create.py the first tool

---
+++ create.py
@@ -0,0 +1,18 @@
+"""
+Let there be light.
+
+User creation tool.
+"""
+
+import sys
+
+def main(args):
+    """
+    Process a file contain a list of user accounts to create.
+    """
+
+    # Make it of the format create.py <command> <args>?
+    pass
+
+if __name__ == "__main__":
+    main(sys.argv)
