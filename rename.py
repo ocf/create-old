9a5ed08bcb824e7ee0ba34c8c5a08200d6bafc35
Nader Morshed <morshed.nader@gmail.com>
2013-03-08 19:18:17 -0800

    More stubbing

--- rename.py
+++ rename.py
@@ -62,7 +62,47 @@ def _add_postgresql(user, new_username, dump):
     """
     pass
 
+def _add_mysql(user, new_username, dump):
+    """
+    Adds a user's mysql tables back into the OCF database.
+    """
+    # Access the username with user["username"]
+    pass
+
 def _add_crontab(user, new_username, dump):
+    pass
+
+def _add_ldap(user, new_username, dump):
+    pass
+
+def _add_ldap_groups(user, new_username, dump):
+    pass
+
+def _add_pykota(user, new_username, dump):
+    pass
+
+def _add_rt(user, new_username, dump):
+    # We care enough to support staff?
+    pass
+
+def _add_wiki(user, new_username, dump):
+    # We care enough to support staff?
+    pass
+
+def _add_mail(user, new_username, dump):
+    # Is this not all in the homedir?
+    pass
+
+def _add_user_info(user, new_username, dump):
+    pass
+
+def _add_homedir(user, new_username, dump):
+    # Probably want to copy their homedir to a tmp directory...or maybe
+    # we can just forgoe the dump/add paradigm for files
+    pass
+
+def _add_webdir(user, new_username, dump):
+    # See comments in _add_homedir
     pass
 
 def _remove_postgresql(user):
