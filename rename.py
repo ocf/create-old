650a4e10aa2c098abf18c5668a6107eee52fdd41
Nader Morshed <morshed.nader@gmail.com>
2013-03-08 18:56:24 -0800

    Stubbing out rename.py

---
+++ rename.py
@@ -0,0 +1,19 @@
+"""
+For the users who couldn't pick their account name right the first time.
+"""
+
+def _dump_postgresl(user):
+    # Access the username with user["username"]
+    pass
+
+def _add_postgresql(user, new_username, dump):
+    """
+    Re-add a user's postgresql database.
+    """
+    pass
+
+def _remove_postgresql(user):
+    """
+    Remove a user's old postgresql database.
+    """
+    pass
