ee338408c4be192d89e44ac7e9589547a7f84b15
Nader Morshed <morshed.nader@gmail.com>
2013-03-08 18:59:09 -0800

    Added more stubs

--- rename.py
+++ rename.py
@@ -3,7 +3,13 @@ For the users who couldn't pick their account name right the first time.
 """
 
 def _dump_postgresl(user):
+    """
+    Dump a user's postgresql database into one object.
+    """
     # Access the username with user["username"]
+    pass
+
+def _dump_crontab(user):
     pass
 
 def _add_postgresql(user, new_username, dump):
@@ -12,8 +18,14 @@ def _add_postgresql(user, new_username, dump):
     """
     pass
 
+def _add_crontab(user, new_username, dump):
+    pass
+
 def _remove_postgresql(user):
     """
     Remove a user's old postgresql database.
     """
+    pass
+
+def _remove_crontab(user):
     pass
