4cd573955a65b4265682643b9f2bbc140690df5a
Dara Adib <daradib@ocf.berkeley.edu>
2015-02-24 19:20:19 -0800

    Aesthetic cleanup of filter_accounts.py
     - Move similarity function under module level
     - PEP-8, PEP-257 fixes

--- filter_accounts.py
+++ filter_accounts.py
@@ -1,27 +1,30 @@
 """
-Module to filter user accounts into good users, problematic users, and users
-that require manual staff approval.
-"""
+Filter user account requests.
 
-from __future__ import with_statement, print_function
+Separate account requests into good users, problematic users, and users
+that require manual staff approval.
+
+"""
+from __future__ import print_function, with_statement
 
 import os
-from email.mime.text import MIMEText
-from subprocess import PIPE, Popen
-from re import findall
+import re
 from difflib import SequenceMatcher
 from itertools import permutations
+from subprocess import PIPE, Popen
 
 import ldap
-import ocf
 
-from ocf import OCF_DN
-from utils import get_log_entries, fancy_open, write_users, irc_alert
+import ocf
+from utils import fancy_open, get_log_entries, irc_alert, write_users
+
 
 ACCOUNT_REJECTED_LETTER = \
-  os.path.join(os.path.dirname(__file__), "txt", "acct.rejected.letter")
+    os.path.join(os.path.dirname(__file__), "txt", "acct.rejected.letter")
 
-def _staff_approval(user, error_str, accepted, needs_approval, rejected, options):
+
+def _staff_approval(user, error_str, accepted, needs_approval, rejected,
+                    options):
     if not options.interactive:
         needs_approval.append((user, error_str))
         return False
@@ -36,8 +39,8 @@ def _staff_approval(user, error_str, accepted, needs_approval, rejected, options
         accepted.append(user)
         return True
     elif ret in ["n", "no"]:
-        irc_alert(
-            "`{}` ({}) rejected by `{}` ({})".format(user["account_name"], user["owner"],
+        irc_alert("`{}` ({}) rejected by `{}` ({})".format(
+            user["account_name"], user["owner"],
             os.environ.get('SUDO_USER', 'root'), error_str))
         rejected.append((user, error_str))
         return False
@@ -45,13 +48,14 @@ def _staff_approval(user, error_str, accepted, needs_approval, rejected, options
         needs_approval.append((user, error_str))
         return False
 
+
 def _filter_log_duplicates(accepted, needs_approval, rejected, options):
     """
-    Filters users for account names already present in accounts.log.
+    Filter users for account names already present in accounts.log.
 
-    Returns new accepted, needs_approval, and rejected lists.
+    Return new accepted, needs_approval, and rejected lists.
+
     """
-
     accepted_new = []
     needs_approval_new = list(needs_approval)
     rejected_new = list(rejected)
@@ -72,14 +76,16 @@ def _filter_log_duplicates(accepted, needs_approval, rejected, options):
     for user in accepted:
         if log_user_names.get(user["account_name"], 0) > 1:
             _staff_approval(user, "Duplicate account name found in log file",
-                            accepted_new, needs_approval_new, rejected_new, options)
+                            accepted_new, needs_approval_new, rejected_new,
+                            options)
         else:
             accepted_new += user,
 
     return accepted_new, needs_approval_new, rejected_new
 
-def _filter_duplicates(key, error_str, accepted, needs_approval, rejected, options,
-                       unique_function = lambda x: x):
+
+def _filter_duplicates(key, error_str, accepted, needs_approval, rejected,
+                       options, unique_function=lambda x: x):
     accepted_new = []
     needs_approval_new = list(needs_approval)
     rejected_new = list(rejected)
@@ -102,14 +108,15 @@ def _filter_duplicates(key, error_str, accepted, needs_approval, rejected, optio
                 unique_values[value].append(user)
                 continue
 
-            # Duplicate found, add this user and the other duplicates to needs_approval_new
+            # Duplicate found, add this user and the other duplicates to
+            # needs_approval_new.
             for other in unique_values[value]:
-                _staff_approval(other, error_str,
-                                accepted_new, needs_approval_new, rejected_new, options)
+                _staff_approval(other, error_str, accepted_new,
+                                needs_approval_new, rejected_new, options)
 
             unique_values[value] = []
-            _staff_approval(user, error_str,
-                            accepted_new, needs_approval_new, rejected_new, options)
+            _staff_approval(user, error_str, accepted_new, needs_approval_new,
+                            rejected_new, options)
         else:
             unique_values[value] = [user]
 
@@ -118,30 +125,39 @@ def _filter_duplicates(key, error_str, accepted, needs_approval, rejected, optio
 
     return accepted_new, needs_approval_new, rejected_new
 
+
 def _fix_name(real_name):
     return real_name.strip().lower()
 
-def _filter_account_name_duplicates(accepted, needs_approval, rejected, options):
-    return _filter_duplicates("account_name", "Duplicate account name detected",
-                              accepted, needs_approval, rejected, options, _fix_name)
+
+def _filter_account_name_duplicates(accepted, needs_approval, rejected,
+                                    options):
+    return _filter_duplicates("account_name",
+                              "Duplicate account name detected",
+                              accepted, needs_approval, rejected, options,
+                              _fix_name)
+
 
 def _filter_owner_duplicates(accepted, needs_approval, rejected, options):
     return _filter_duplicates("owner", "Duplicate owner detected",
-                              accepted, needs_approval, rejected, options, _fix_name)
+                              accepted, needs_approval, rejected, options,
+                              _fix_name)
 
-def _filter_university_uid_duplicates(accepted, needs_approval, rejected, options):
-    return _filter_duplicates("university_uid", "Duplicate CalNet UID detected",
+
+def _filter_university_uid_duplicates(accepted, needs_approval, rejected,
+                                      options):
+    return _filter_duplicates("university_uid",
+                              "Duplicate CalNet UID detected",
                               accepted, needs_approval, rejected, options)
+
 
 def _filter_email_duplicates(accepted, needs_approval, rejected, options):
     return _filter_duplicates("email", "Duplicate email address detected",
                               accepted, needs_approval, rejected, options)
 
-def _filter_ocf_duplicates(accepted, needs_approval, rejected, options):
-    """
-    Search the OCF ldap database for matching cn entries.
-    """
 
+def _filter_ocf_duplicates(accepted, needs_approval, rejected, options):
+    """Search the OCF LDAP database for matching cn entries."""
     retrieve_attrs = ["uidNumber", "cn"]
 
     accepted_new = []
@@ -155,25 +171,27 @@ def _filter_ocf_duplicates(accepted, needs_approval, rejected, options):
 
         field = "callinkOid" if user["is_group"] else "calnetUid"
         search_filter = "{0}={1}".format(field, user["university_uid"])
-        results = options.ocf_ldap.search_st(OCF_DN, ldap.SCOPE_SUBTREE,
+        results = options.ocf_ldap.search_st(ocf.OCF_DN, ldap.SCOPE_SUBTREE,
                                              search_filter, retrieve_attrs)
+        uidnumber = results[0][1]["uidNumber"][0]
 
-        if (results and
-            results[0][1]["uidNumber"][0] >= options.conflict_uid_lower_bound):
+        if (results and uidnumber >= options.conflict_uid_lower_bound):
             _staff_approval(user, "Possible existing account",
-                            accepted_new, needs_approval_new, rejected_new, options)
+                            accepted_new, needs_approval_new, rejected_new,
+                            options)
         else:
             accepted_new += user,
 
     return accepted_new, needs_approval_new, rejected_new
+
 
 def _filter_registration_status(accepted, needs_approval, rejected, options):
     """
     Check member eligibility by their CalNet registration status.
 
     See rules: http://wiki.ocf.berkeley.edu/membership/eligibility/
-    """
 
+    """
     base_dn = "dc=berkeley,dc=edu"
     retrieve_attrs = ["berkeleyEduAffiliations", "displayName"]
 
@@ -198,18 +216,20 @@ def _filter_registration_status(accepted, needs_approval, rejected, options):
 
         affiliation = results[0][1]["berkeleyEduAffiliations"]
 
-        allowed_affiliates = set(["AFFILIATE-TYPE-CONSULTANT",
-                                  "AFFILIATE-TYPE-LBLOP STAFF",
-                                  "AFFILIATE-TYPE-VISITING SCHOLAR",
-                                  "AFFILIATE-TYPE-VOLUNTEER",
-                                  "AFFILIATE-TYPE-HHMI RESEARCHER",
-                                  "AFFILIATE-TYPE-VISITING STU RESEARCHER",
-                                  "AFFILIATE-TYPE-LBL/DOE POSTDOC",
-                                  "AFFILIATE-TYPE-TEMP AGENCY",
-                                  "AFFILIATE-TYPE-COMMITTEE MEMBER",
-                                  "AFFILIATE-TYPE-STAFF OF UC/OP/AFFILIATED ORGS",
-                                  "AFFILIATE-TYPE-CONTRACTOR",
-                                  "AFFILIATE-TYPE-CONCURR ENROLL",])
+        allowed_affiliates = set([
+            "AFFILIATE-TYPE-CONSULTANT",
+            "AFFILIATE-TYPE-LBLOP STAFF",
+            "AFFILIATE-TYPE-VISITING SCHOLAR",
+            "AFFILIATE-TYPE-VOLUNTEER",
+            "AFFILIATE-TYPE-HHMI RESEARCHER",
+            "AFFILIATE-TYPE-VISITING STU RESEARCHER",
+            "AFFILIATE-TYPE-LBL/DOE POSTDOC",
+            "AFFILIATE-TYPE-TEMP AGENCY",
+            "AFFILIATE-TYPE-COMMITTEE MEMBER",
+            "AFFILIATE-TYPE-STAFF OF UC/OP/AFFILIATED ORGS",
+            "AFFILIATE-TYPE-CONTRACTOR",
+            "AFFILIATE-TYPE-CONCURR ENROLL",
+        ])
 
         if ((("EMPLOYEE-TYPE-ACADEMIC" in affiliation or
               "EMPLOYEE-TYPE-STAFF" in affiliation) and
@@ -225,10 +245,11 @@ def _filter_registration_status(accepted, needs_approval, rejected, options):
             message = "CalNet status not eligible for account ({0})"
             message = message.format(", ".join(affiliation))
 
-            _staff_approval(user, message,
-                            accepted_new, needs_approval_new, rejected_new, options)
+            _staff_approval(user, message, accepted_new, needs_approval_new,
+                            rejected_new, options)
 
     return accepted_new, needs_approval_new, rejected_new
+
 
 def _filter_restricted_names(accepted, needs_approval, rejected, options):
     accepted_new = []
@@ -236,10 +257,9 @@ def _filter_restricted_names(accepted, needs_approval, rejected, options):
     rejected_new = list(rejected)
 
     # Some bad words, not comprehensive
-    bad = {
-        "expletive": ["fuck", "shit", "cunt", "crap", "bitch", "hell", "ass", "dick"],
-        "restricted": ["ocf", "ucb", "cal"],
-        }
+    bad = {"expletive": ["fuck", "shit", "cunt", "crap", "bitch", "hell",
+                         "ass", "dick"],
+           "restricted": ["ocf", "ucb", "cal"]}
 
     for user in accepted:
         if any(word in user["account_name"]
@@ -261,34 +281,8 @@ def _filter_restricted_names(accepted, needs_approval, rejected, options):
 
     return accepted_new, needs_approval_new, rejected_new
 
+
 def _filter_real_names(accepted, needs_approval, rejected, options):
-
-    def similarity(realname, username):
-        """Find the number of additions needed when turning realname into username.
-
-        Find the minimum number of replacements and insertions (deletions are not counted)
-        that turn any of the permutations of words in realname into username, using the
-        built-in difflib.SequenceMatcher class. SequenceMatcher finds the longest
-        continguous matching subsequence and continues this process recursively.
-
-        This is usually, but not always, the edit distance with zero deletion cost.
-        Long realnames combined with very short matching subsequences (such as initials)
-        may require more additions with SequenceMatcher than their edit distances.
-
-        For most usernames based on real names, this number is 0.
-        """
-        distances = []
-
-        for permutation in permutations(findall("\w+", realname)):
-            permutation = "".join(permutation).lower()
-            s = SequenceMatcher(None, permutation, username)
-            edits = s.get_opcodes()
-            distance = sum(edit[4] - edit[3]
-                           for edit in edits
-                           if edit[0] in ["replace", "insert"])
-            distances.append(distance)
-
-        return sorted(distances)[0]
 
     accepted_new = []
     needs_approval_new = list(needs_approval)
@@ -309,23 +303,53 @@ def _filter_real_names(accepted, needs_approval, rejected, options):
 
     return accepted_new, needs_approval_new, rejected_new
 
+
 def _send_rejection_mail(rejected, options):
     if rejected and options.email:
         rejected_text = open(ACCOUNT_REJECTED_LETTER).read()
         os.environ["REPLYTO"] = ocf.MAIL_FROM_HELP
 
         for user, comment in rejected:
-            body = rejected_text.format(account_name = user["account_name"],
-                                       comment = comment)
-            s = Popen(["mail", "-a", "From: " + ocf.MAIL_FROM_HELP, "-s", "OCF Account Request Rejected", user["email"]],
-                      stdin = PIPE)
+            body = rejected_text.format(account_name=user["account_name"],
+                                        comment=comment)
+            s = Popen(["mail", "-a", "From: " + ocf.MAIL_FROM_HELP, "-s",
+                       "OCF Account Request Rejected", user["email"]],
+                      stdin=PIPE)
             s.communicate(body)
 
-def filter_accounts(users, options):
-    """
-    Filter accounts into auto-accepted, needs-staff-approval, and rejected.
-    """
 
+def similarity(realname, username):
+    """Find the number of edits needed when turning realname into username.
+
+    Find the minimum number of replacements and insertions
+    (deletions are not counted) that turn any of the permutations of words in
+    realname into username, using the built-in difflib.SequenceMatcher class.
+    SequenceMatcher finds the longest continguous matching subsequence and
+    continues this process recursively.
+
+    This is usually, but not always, the edit distance with zero deletion
+    cost. Long realnames combined with very short matching subsequences
+    (such as initials) may require more additions with SequenceMatcher than
+    their edit distances.
+
+    For most usernames based on real names, this number is 0.
+    """
+    distances = []
+
+    for permutation in permutations(re.findall("\w+", realname)):
+        permutation = "".join(permutation).lower()
+        s = SequenceMatcher(None, permutation, username)
+        edits = s.get_opcodes()
+        distance = sum(edit[4] - edit[3]
+                       for edit in edits
+                       if edit[0] in ["replace", "insert"])
+        distances.append(distance)
+
+    return sorted(distances)[0]
+
+
+def filter_accounts(users, options):
+    """Filter accounts into accepted, needs-staff-approval, and rejected."""
     accepted = list(users)
     needs_approval = []
     rejected = []
@@ -336,15 +360,17 @@ def filter_accounts(users, options):
 
     # Check for account name duplicates
     accepted, needs_approval, rejected = \
-      _filter_account_name_duplicates(accepted, needs_approval, rejected, options)
+        _filter_account_name_duplicates(accepted, needs_approval, rejected,
+                                        options)
 
     # Check for owner duplicates
     accepted, needs_approval, rejected = \
-      _filter_owner_duplicates(accepted, needs_approval, rejected, options)
+        _filter_owner_duplicates(accepted, needs_approval, rejected, options)
 
     # Check for CalNet UID duplicates
     accepted, needs_approval, rejected = \
-      _filter_university_uid_duplicates(accepted, needs_approval, rejected, options)
+        _filter_university_uid_duplicates(accepted, needs_approval, rejected,
+                                          options)
 
     # Check for email address duplicates
     # accepted, needs_approval, rejected = \
@@ -352,22 +378,24 @@ def filter_accounts(users, options):
 
     # Check for OCF existing account duplicates
     accepted, needs_approval, rejected = \
-      _filter_ocf_duplicates(accepted, needs_approval, rejected, options)
+        _filter_ocf_duplicates(accepted, needs_approval, rejected,
+                               options)
 
     # Check CalNet registration status
     accepted, needs_approval, rejected = \
-      _filter_registration_status(accepted, needs_approval, rejected, options)
+        _filter_registration_status(accepted, needs_approval, rejected,
+                                    options)
 
     # Check for expletives and restrictions in requested usernames
     accepted, needs_approval, rejected = \
-      _filter_restricted_names(accepted, needs_approval, rejected, options)
+        _filter_restricted_names(accepted, needs_approval, rejected, options)
 
     # Check that requested username is based on real name
     accepted, needs_approval, rejected = \
-      _filter_real_names(accepted, needs_approval, rejected, options)
+        _filter_real_names(accepted, needs_approval, rejected, options)
 
     # Write the accepted users to a staging file, allowing them marinate
-    with fancy_open(options.mid_approve, "a", lock = True) as f:
+    with fancy_open(options.mid_approve, "a", lock=True) as f:
         write_users(f, accepted)
 
     # Email out this information
