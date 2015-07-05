c446030abf4a7a0196e9583a2d9128eee3e60b2b
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2015-07-05 16:10:58 -0700

    Cleanup, start using ocflib

--- ocf.py
+++ ocf.py
@@ -11,24 +11,8 @@ from time import asctime
 
 from utils import fancy_open
 
-OCF_DN = "ou=People,dc=OCF,dc=Berkeley,dc=EDU"
-
 MAIL_FROM_HELP = "Open Computing Facility <help@ocf.berkeley.edu>"
 MAIL_FROM_BOT = "OCF Account Creation Bot <root@ocf.berkeley.edu>"
-
-def home_dir(account_name):
-    """
-    Returns the user's home directory: "/home/u/us/account_name".
-    """
-    return os.path.sep + \
-      os.path.join("home", account_name[0], account_name[:2], account_name)
-
-def http_dir(account_name):
-    """
-    Returns the user's http directory: "/services/http/users/u/account_name".
-    """
-    return os.path.sep + \
-      os.path.join("services", "http", "users", account_name[0], account_name)
 
 def log_creation(user, options):
     with fancy_open(options.log_file, "a", lock = True) as f:
